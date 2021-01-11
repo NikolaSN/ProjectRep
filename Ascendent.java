33be69e954331afc377e2ef1f407297045936ef7
NikolaSN <74970423+NikolaSN@users.noreply.github.com>
2021-01-11 21:07:03 +0200

    Moje da pregledash i dadesh suveti
    eto kakvo sum napravil do sega
    nqma da otkaja pomosht za formulata kak da se napravi algorituma i posle trqbva da izmislim kak kato poluchim rezultat ot formulata da go svurzvame s opredeleniq ascendent

--- Ascendent.java
+++ Ascendent.java
@@ -11,6 +11,8 @@ import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
 import java.util.InputMismatchException;
 import java.awt.event.ActionEvent;
+import java.awt.TextArea;
+import java.awt.Color;
 
 public class Ascendent extends JFrame {
 
@@ -48,6 +50,18 @@ public class Ascendent extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		TextArea textZodiq = new TextArea();
+		textZodiq.setBackground(Color.WHITE);
+		textZodiq.setEditable(false);
+		textZodiq.setBounds(27, 357, 343, 108);
+		contentPane.add(textZodiq);
+		
+		TextArea textAsc = new TextArea();
+		textAsc.setBackground(Color.WHITE);
+		textAsc.setEditable(false);
+		textAsc.setBounds(418, 10, 343, 359);
+		contentPane.add(textAsc);
+		
 		Day = new JTextField();
 		Day.setBounds(21, 71, 110, 32);
 		contentPane.add(Day);
@@ -73,10 +87,6 @@ public class Ascendent extends JFrame {
 		contentPane.add(Minutes);
 		Minutes.setColumns(10);
 		
-		JLabel lblAsc = new JLabel("");
-		lblAsc.setBounds(440, 21, 322, 353);
-		contentPane.add(lblAsc);
-		
 		JButton startbtn = new JButton("Ascedent!");
 		startbtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -88,9 +98,9 @@ public class Ascendent extends JFrame {
 					hour= Integer.parseInt(Hours.getText());
 					minutes= Integer.parseInt(Minutes.getText());
 				}catch(NumberFormatException e) {
-					System.out.println("Въведете число моля");
+					textZodiq.setText("Vuvedete chislo molq");
 				}catch(InputMismatchException e) {
-					System.out.println("Не може да бъде сведено до цяло число");
+					textZodiq.setText("Ne moje da bude svedeno do cqlo chislo");
 				}
 				
 			}
@@ -113,10 +123,91 @@ public class Ascendent extends JFrame {
 		JLabel lblYear = new JLabel("Year");
 		lblYear.setBounds(299, 38, 92, 26);
 		contentPane.add(lblYear);
-	}
+		
+		JLabel lblWarning = new JLabel("Please use valid numbers");
+		lblWarning.setBounds(21, 0, 388, 26);
+		contentPane.add(lblWarning);
+		
 	
-	private void Formula() {
+		
 		
 	}
 	
+	public void Zodiq() {
+		//1-oven; 2- telec; 3-bliznaci; 4-rak; 5-luv; 6-deva; 7-vezni; 8-skorpion; 9- strelec; 10-kozirog; 11-vodolei; 12-ribi;
+		int day, month, year, hour, minutes, zodiq;
+		day= Integer.parseInt(Day.getText());
+		month= Integer.parseInt(Month.getText());
+		year= Integer.parseInt(Year.getText());
+		hour= Integer.parseInt(Hours.getText());
+		minutes= Integer.parseInt(Minutes.getText());
+		if (month == 3 && day>20 || month == 4 && day <21) {
+			textZodiq.setText("Vashata zodiq e Oven");
+			zodiq=1;
+		}
+		if (month == 4 && day>20 || month == 5 && day <22) {
+			textZodiq.setText("Vashata zodiq e Telec");
+			zodiq=2;
+		}
+		if (month == 5 && day>21 || month == 6 && day <22) {
+			textZodiq.setText("Vashata zodiq e Bliznaci");
+			zodiq=3;
+		}
+		if (month == 6 && day>21 || month == 7 && day <23) {
+			textZodiq.setText("Vashata zodiq e Rak");
+			zodiq=4;
+		}
+		if (month == 7 && day>22 || month == 8 && day <24) {
+			textZodiq.setText("Vashata zodiq e Lav");
+			zodiq=5;
+		}
+		if (month == 8 && day>23 || month == 9 && day <24) {
+			textZodiq.setText("Vashata zodiq e Deva");
+			zodiq=6;
+		}
+		if (month == 9 && day>23 || month == 10 && day <24) {
+			textZodiq.setText("Vashata zodiq e Vezni");
+			zodiq=7;
+			}
+		if (month == 10 && day>23 || month == 11 && day <23) {
+			textZodiq.setText("Vashata zodiq e Skorpion");
+			zodiq=8;
+			}
+		if (month == 11 && day>22 || month == 12 && day <22) {
+			textZodiq.setText("Vashata zodiq e Strelec");
+			zodiq=9;
+			}
+		if (month == 12 && day>21 || month == 1 && day <21) {
+			textZodiq.setText("Vashata zodiq e Kozirog");
+			zodiq=10;
+			}
+		if (month == 1 && day>20 || month == 2 && day <20) {
+			textZodiq.setText("Vashata zodiq e Vodolei");
+			zodiq=11;
+			}
+		if (month == 2 && day>19 || month == 3 && day <21) {
+			textZodiq.setText("Vashata zodiq e Ribi");
+			zodiq=12;
+			}
+	}
+
+	public void Formula() {
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- ValidationClass.java
+++ ValidationClass.java
@@ -5,5 +5,74 @@ public class ValidationClass extends Ascendent{
 		// TODO Auto-generated method stub
 
 	//}
+	private int day, month, year, hour, minutes ;
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day1) {
+        if (day1 < 32 && day1 > 0) {
+            this.day = day1;
+        } else {
+            throw new NumberFormatException();
+        }
+    }
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month1) {
+        if (month1 < 13 && month1 > 0) {
+            this.month = month1;
+        } else {
+            throw new NumberFormatException();
+        }
+        }
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year1) {
+        if (year1 < 1859 && year1 > 2100) {
+            this.year = year1;
+        } else {
+            throw new NumberFormatException();
+        }
+    }    
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour1) {
+        if (hour1 < 25) {
+            this.hour = hour1;
+        } else {
+            throw new NumberFormatException();
+        }
+    }   
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes1) {
+        if (minutes1 < 60) {
+            this.minutes = minutes1;
+        } else {
+            throw new NumberFormatException();
+        }
+    }   
+
+    public ValidationClass(int day, int month, int year, int hour, int minutes) {
+    	setDay(day);
+    	setMonth(month);
+    	setYear(year);
+    	setHour(hour);
+    	setMinutes(minutes);
+        }
+
+
+
 
 }
+
